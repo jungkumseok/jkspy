f873eff5276a9dd959e762cacc34e14d24ce5a79
Kumseok Jung <jungkumseok@gmail.com>
2016-01-15 14:43:06 -0800

    - changed version in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'jkspy',
   packages = ['jkspy'], # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Python utilities for doing computer stuff',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
   url = 'https://github.com/jungkumseok/jkspy',
-  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.3.tar.gz',
   keywords = ['testing', 'utility'], 
   classifiers = [],
 )
