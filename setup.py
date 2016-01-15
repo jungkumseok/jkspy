e47bd9149494833ac15d70510fc4c771f75a19c8
Kumseok Jung <jungkumseok@gmail.com>
2016-01-15 15:26:52 -0800

    Renamed LICENSE to LICENCE.txt

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'jkspy',
   packages = ['jkspy'],
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'Python utilities for doing computer stuff',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
+  license = 'LICENSE.txt',
   url = 'https://github.com/jungkumseok/jkspy',
-  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.5.tar.gz',
+  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.6.tar.gz',
   keywords = ['testing', 'utility'], 
   classifiers = [],
 )
