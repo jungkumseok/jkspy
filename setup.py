50c483eac7da97ddacff10c5d2318b314f112f05
Kumseok Jung <jungkumseok@gmail.com>
2016-01-15 15:12:10 -0800

    Added MANIFEST and README

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'jkspy',
-  packages = ['jkspy'], # this must be the same as the name above
-  version = '0.1.3',
+  packages = ['jkspy'],
+  version = '0.1.4',
   description = 'Python utilities for doing computer stuff',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
   url = 'https://github.com/jungkumseok/jkspy',
-  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.3.tar.gz',
+  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.4.tar.gz',
   keywords = ['testing', 'utility'], 
   classifiers = [],
 )
