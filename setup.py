4acdf78876d5ba3eaa86dda96741c78170eb9476
Kumseok Jung <jungkumseok@gmail.com>
2016-01-15 15:20:48 -0800

    Removed Eclipse metadata files from repo

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'jkspy',
   packages = ['jkspy'],
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'Python utilities for doing computer stuff',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
   url = 'https://github.com/jungkumseok/jkspy',
-  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.4.tar.gz',
+  download_url = 'https://github.com/jungkumseok/jkspy/archive/0.1.5.tar.gz',
   keywords = ['testing', 'utility'], 
   classifiers = [],
 )
